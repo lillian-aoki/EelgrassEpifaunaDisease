30da380c715abef5ce38176019ab63388d76410d
lillian-aoki <lra53@cornell.edu>
2024-03-20 08:54:08 -0700

    Add relative abundance supplemental figure

--- code/02_supplemental_figures.R
+++ code/02_supplemental_figures.R
@@ -2,9 +2,11 @@
 
 # Last updated: 2024-03-19 by Lillian Aoki
 
-## This script produces figures for (1) the distribution of epifauna across latitudes,
+## This script produces figures for 
+## (1) the distribution of epifauna across latitudes,
 ## (2) comparison of seagrass structure and disease prevalence, 
-## and (3) a map of study locations with temperature. 
+## (3) a map of study locations with temperature 
+## (4) relative abundace of eelgrass grazers within epifauna community
 
 library(tidyverse)
 library(patchwork)
@@ -17,6 +19,9 @@ region_order <- c("AK", "BC", "WA", "OR", "BB", "SD")
 site_dat <- read_csv("data/output/epifauna_site_for_plotting.csv")
 site_dat$fYear <- ordered(as.factor(site_dat$Year), levels = c("2019", "2020", "2021"))
 site_dat$Region <- ordered(site_dat$Region, levels=region_order)
+
+epi <- read_csv("data/input/EGWD_transect_data_v20230307_big_epi_with_BB_transect.csv")
+meta <- read_csv("data/input/combined_site_metadata.csv")
 
 # epifauna distributions ####
 amp_plot <- ggplot(site_dat, aes(x=Latitude, y=Ampithoid_large, color=fYear))+
@@ -92,7 +97,7 @@ ggplot(site_dat, aes(x=Density, y=CanopyHeight, color=Region, shape=fYear, size=
 # ggsave("figures/seagrass_structure_prevalence.jpg", width = 4, height = 6)
 ggsave("figures/seagrass_structure_prevalence.tiff", width = 4, height = 6)
 
-## site map ####
+# site map ####
 sites <- na.omit(site_dat)
 sites <- subset(sites, Year=="2019")
 sites_sf <- st_as_sf(sites, coords = c("Longitude", "Latitude"), crs = 4326, agr = "constant")
@@ -125,3 +130,60 @@ mapFull +
 
 ggsave("figures/map_study_sites.jpg", width = 4, height = 6)
 ggsave("figures/map_study_sites.tiff", width = 4, height = 6)
+
+# relative abundance of eelgrass grazers ####
+#transect level epifauna - need to collapse to site
+meta19 <- subset(meta, Year=="2019")
+loc19 <- select(meta19, c("Region","SiteCode", "TidalHeight", "Transect", "TransectBeginDecimalLatitude",
+                          "TransectBeginDecimalLongitude", "TransectEndDecimalLatitude", "TransectEndDecimalLongitude"))
+full_meta <- meta %>%
+  rows_update(loc19, by = c("Region", "SiteCode", "TidalHeight", "Transect"))
+meta_site <- full_meta %>%
+  group_by(Year, Region, SiteCode) %>%
+  summarise(Latitude=mean(as.numeric(TransectBeginDecimalLatitude)), Longitude=mean(TransectBeginDecimalLongitude))
+
+epi$site <- str_extract(epi$site_unique_code, "\\.[A-F]\\.")
+epi$site <- gsub("\\.", "", epi$site)
+epi <- left_join(epi, meta_site, by=c("year"="Year", "region" = "Region", "site" = "SiteCode"))
+epi$region <- ordered(epi$region, levels=region_order)
+epi_summ <- epi %>%
+  group_by(year, region, site, site_unique_code, Latitude, meadow=paste(region, site, sep="_")) %>%
+  mutate(meadow=paste(region, site, sep="_"))%>%
+  summarise(total_abun_large=mean(epifauna_per_g_transect_large),
+            lac_abun_large=mean(lacuna_per_g_transect_large),
+            amp_abun_large=mean(ampithoid_per_g_transect_large),
+            ido_abun_large=mean(idoteid_per_g_transect_large),
+            other_abun_large=total_abun_large-sum(lac_abun_large, amp_abun_large, ido_abun_large))
+epi_summ_long <- pivot_longer(epi_summ, cols = c("other_abun_large", "lac_abun_large", "amp_abun_large", "ido_abun_large"),
+                              names_to = "taxon") %>%
+  arrange(Latitude)
+meadow_order <- c("AK_A", "AK_B", "AK_C", "AK_D", "AK_E", "AK_F",
+                  "BC_A", "BC_B", "BC_C", "BC_D", "BC_E",
+                  "WA_A", "WA_B", "WA_C", "WA_D", "WA_E", 
+                  "OR_A", "OR_B", "OR_C", "OR_D", "OR_E", 
+                  "BB_A", "BB_B", "BB_C", "BB_D", "BB_E", "BB_F",
+                  "SD_A", "SD_B", "SD_C", "SD_D", "SD_E")
+epi_summ_long$meadow <- ordered(epi_summ_long$meadow, levels=meadow_order)
+epi_summ$other_per <- epi_summ$other_abun_large/epi_summ$total_abun_large
+epi_summ$gz_per <- 1-epi_summ$other_per
+epi_summ$lac_per <- epi_summ$lac_abun_large/epi_summ$total_abun_large
+epi_summ$ido_per <- epi_summ$ido_abun_large/epi_summ$total_abun_large
+epi_summ$amp_per <- epi_summ$amp_abun_large/epi_summ$total_abun_large
+summary(epi_summ$gz_per)
+epi_summ %>% filter(gz_per>0.5) %>% filter(total_abun_large>1) %>% select(c(site_unique_code, lac_per, ido_per, amp_per))
+
+ggplot(epi_summ_long, aes(x=meadow, y=value, fill=taxon))+
+  geom_rect(aes(xmin="BB_A", xmax="BB_F", ymin=0, ymax=20), linetype="dashed", fill="grey90")+
+  geom_col()+
+  facet_grid(rows = "year")+
+  ylab("Count of animals per g macrophytes")+
+  scale_fill_viridis_d(labels=c("ampithiod \namphipods", "idoteid \nisopods", expression(atop(italic("Lacuna"), "snails")), "other \ntaxa"))+
+  scale_y_continuous(expand = c(0,0))+
+  theme_bw(base_size=14)+
+  theme(panel.grid = element_blank(),
+        axis.text.x = element_text(angle=45, vjust = 1, size=9, hjust = 1),
+        legend.position = "bottom",
+        legend.title = element_blank(),
+        legend.text = element_text(hjust = 0))
+
+ggsave("figures/rel_abun.tiff", width=8, height = 6)
